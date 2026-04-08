734c3744c16ee0d7f9b379e9a895cefb99199568
Wajza <s202262140@kfupm.edu.sa>
2026-04-08 11:11:08 +0300

    TODO4&5

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -109,7 +109,6 @@ import express from "express";
 
 // create express app instance to create web server
 const app = express();
-app.listen(3000, ()=> console.log("API running at http://localhost:3000"));;
 
 // Query params: /echo?name=Ali&age=22
 app.get("/echo", (req,res)=>{
@@ -129,11 +128,21 @@ app.get("/profile/:first/:last", (req,res)=>{
 });
 
 // Route param middleware example: /users/42
+app.param("userId", (req,res,next,userId)=>{
+  const userIdNum = Number(userId);
+  if (isNaN(userIdNum) || userIdNum <= 0) {
+    return res.status(400).json({ ok:false, error:"userId must be positive number" });
+  }
+  req.userIdNum = userIdNum;
+  next();
+});
 
 
 // Route params: /users/:userId route
-
+app.get("/users/:userId", (req,res)=>{
+  res.json({ ok:true, userId: req.userIdNum });
+});
 
 // Start the server by listening
-
+app.listen(3000, ()=> console.log("API running at http://localhost:3000"));
 
